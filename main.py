40da97ae4f98b037865e04dbbc455773c8f0d685
Harshal19t <harshal1901@gmail.com>
2026-08-03 19:06:04 +0100

    Query Parameters

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
-@app.get("/") #decorate
-def index():
+@app.get("/blog") #decorate
+def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
     return {"data": {"name":"Hello, World!"}}
 
 @app.get("/unpublished")
@@ -15,5 +16,5 @@ def show(id: int):
     return {"data": {"name":"About Us", "id": id}}
 
 @app.get("/blog/{id}/comments")
-def comments(id: int):
-    return {"data": {"name":"Comments", "id": id}}
+def comments(id: int, limit: int = 10):
+    return {"data": {"name":"Comments", "id": id, "limit": limit}}
