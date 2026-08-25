462603d441d10302891a56e05524fb5c669aad89
Harshal19t <harshal1901@gmail.com>
2026-08-25 02:44:32 +0100

    basic

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
 from fastapi import FastAPI
-from typing import Optional
 
 app = FastAPI()
 
-@app.get("/blog") #decorate
-def index(limit: int = 10, published: bool = True, sort: Optional[str] = None):
-    return {"data": {"name":"Hello, World!"}}
-
-@app.get("/unpublished")
-def unpublished():
-    return {"data": {"name":"Unpublished"}}
-
-@app.get("/blog/{id}") 
-def show(id: int):
-    return {"data": {"name":"About Us", "id": id}}
-
-@app.get("/blog/{id}/comments")
-def comments(id: int, limit: int = 10):
-    return {"data": {"name":"Comments", "id": id, "limit": limit}}
+@app.get("/")
+async def read_root():
+    return {"message":"Hello World"}
