e6352284ba60a0c1c1edea696abd6d0d0d795432
Harshal19t <harshal1901@gmail.com>
2026-08-27 03:21:16 +0100

    pre-defined valus

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+#basic
 from fastapi import FastAPI
-
 app = FastAPI()
+
+from enum import Enum
 
 @app.get("/")
 async def read_root():
     return {"message":"Hello World"}
 
+# path parameter
 @app.get("/items/{item_id}")
 async def read_item(item_id: int):
     return {"item_id": item_id}
+
+# order
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+# pre-deined values
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+    
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    
+    # if model_name is ModelName.alexnet:
+    #     return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    if ModelName.alexnet == model_name:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}  
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}    
